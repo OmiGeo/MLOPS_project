600a3b04dfd379eba2cc64c5aad32df9f14c7220
OmiGeo <umashankarkandpal612@gail.com>
2023-12-30 11:17:43 +0530

    adding content

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a mlops end to end project',
+    author='OmiGeo',
+    license='MIT',
+)
